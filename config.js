4dfb89aae7af1ff9eebec54e3bced834235eec2d
Funyegg <97416819+Funyegg@users.noreply.github.com>
2022-02-13 23:26:22 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+//Server Port
+module.exports.port = 6969;
+
+//List of skins (save them as .png files)
+module.exports.skins = ["bonz","jabb","monkey","danieltur","ard","allah"];
+
+//Max clients (set to 0 for no limit)
+module.exports.max = 0;
+
+//Max clients per ip (set to 0 for no limit)
+module.exports.maxip = 5;
